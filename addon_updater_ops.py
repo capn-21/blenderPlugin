9915e2c8862ff920694cd39d1198f6af6cc9df53
marinouj <marinoujoanna@gmail.com>
2022-04-12 14:32:44 +0100

    adjustments

--- addon_updater_ops.py
+++ addon_updater_ops.py
@@ -1357,12 +1357,12 @@ def register(bl_info):
 
     # Choose your own repository, must match git name for GitHUb and Bitbucket,
     # for GitLab use project ID (numbers only).
-    updater.repo = "blender_plugin"
+    updater.repo = "blenderPlugin"
 
     # updater.addon = # define at top of module, MUST be done first
 
     # Website for manual addon download, optional but recommended to set.
-    updater.website = "https://github.com/Kaedim/blender_plugin"
+    updater.website = "https://github.com/Kaedim/blenderPlugin"
 
     # Addon subfolder path.
     # "sample/path/to/addon"
